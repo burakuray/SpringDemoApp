851f927911b93ae9a4db0e4de802128b5862e637
Burak URAY <burak.uray@gmail.com>
2021-05-30 18:55:57 +0300

    Add dao some JpaRepository implementation

--- src/main/java/com/buray/northwind/entities/concretes/Product.java
+++ src/main/java/com/buray/northwind/entities/concretes/Product.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -23,8 +25,8 @@ public class Product {
 	@Column(name="product_id")
 	private int id;
 	
-	@Column(name="category_id")
-	private int categoryId;
+	//@Column(name="category_id")
+	//private int categoryId;
 	
 	@Column(name="product_name")
 	private String productName;
@@ -40,5 +42,9 @@ public class Product {
 	
 	@Column(name="units_on_order")
 	private short unitsInOrder;
+	
+	@ManyToOne()
+	@JoinColumn(name="category_id")
+	private Category category;
 	
 }
